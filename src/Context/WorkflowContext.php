736dd3d951975c45342c6a6b5eb6de1237d05f39
TravisCarden <travis.carden@acquia.com>
2018-07-09 20:25:42 -0500

    Added workflow context.

---
+++ src/Context/WorkflowContext.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Acquia\DrupalSpecTool\Context;
+
+use Behat\Gherkin\Node\TableNode;
+use TravisCarden\BehatTableComparison\TableEqualityAssertion;
+
+/**
+ * Provides content model Behat step definitions.
+ */
+class WorkflowContext extends BaseContext {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private $entityTypeManager;
+
+  /**
+   * The workflow type plugin manager.
+   *
+   * @var \Drupal\workflows\WorkflowTypeManager
+   */
+  private $workflowTypePluginManager;
+
+  /**
+   * Constructs a ContentModelContext.
+   */
+  public function __construct() {
+    $this->entityTypeManager = \Drupal::entityTypeManager();
+    $this->workflowTypePluginManager = \Drupal::service('plugin.manager.workflows.type');
+  }
+
+  /**
+   * Asserts the configuration of workflows.
+   *
+   * @Then exactly the following workflows should exist
+   *
+   * @throws \Exception
+   */
+  public function assertWorkflows(TableNode $expected) {
+    $workflow_info = [];
+    foreach ($this->getWorkflows() as $workflow) {
+      $workflow_info[] = [
+        $workflow->label(),
+        $workflow->id(),
+        (string) $workflow->getTypePlugin()->label(),
+      ];
+    }
+    $actual = new TableNode($workflow_info);
+
+    (new TableEqualityAssertion($expected, $actual))
+      ->expectHeader([
+        'Label',
+        'Machine name',
+        'Type',
+      ])
+      ->ignoreRowOrder()
+      ->setMissingRowsLabel(self::missingRowsLabelFor('workflow'))
+      ->setUnexpectedRowsLabel(self::unexpectedRowsLabelFor('workflow'))
+      ->assert();
+  }
+
+  /**
+   * Asserts the configuration of workflow states.
+   *
+   * @Then exactly the following workflow states should exist
+   *
+   * @throws \Exception
+   */
+  public function assertWorkflowStates(TableNode $expected) {
+    $states_info = [];
+    foreach ($this->getWorkflows() as $workflow) {
+      /** @var \Drupal\workflows\StateInterface[] $states */
+      $states = $workflow->getTypePlugin()->getStates();
+      foreach ($states as $state) {
+        $states_info[] = [
+          $workflow->label(),
+          $state->label(),
+          $state->id(),
+        ];
+      }
+    }
+    $actual = new TableNode($states_info);
+
+    (new TableEqualityAssertion($expected, $actual))
+      ->expectHeader([
+        'Workflow',
+        'Label',
+        'Machine name',
+      ])
+      ->ignoreRowOrder()
+      ->setMissingRowsLabel(self::missingRowsLabelFor('workflow states'))
+      ->setUnexpectedRowsLabel(self::unexpectedRowsLabelFor('workflow states'))
+      ->assert();
+  }
+
+  /**
+   * Asserts the configuration of workflow transitions.
+   *
+   * @Then exactly the following workflow transitions should exist
+   *
+   * @throws \Exception
+   */
+  public function assertWorkflowTransitions(TableNode $expected) {
+    $states_info = [];
+    foreach ($this->getWorkflows() as $workflow) {
+
+      /** @var \Drupal\workflows\TransitionInterface[] $transitions */
+      $transitions = $workflow->getTypePlugin()->getTransitions();
+      foreach ($transitions as $transition) {
+        foreach ($transition->from() as $from_state) {
+
+          $states_info[] = [
+            $workflow->label(),
+            $transition->label(),
+            $transition->id(),
+            $from_state->label(),
+            $transition->to()->label(),
+          ];
+        }
+
+      }
+    }
+    $actual = new TableNode($states_info);
+
+    (new TableEqualityAssertion($expected, $actual))
+      ->expectHeader([
+        'Workflow',
+        'Label',
+        'Machine name',
+        'From state',
+        'To state',
+      ])
+      ->ignoreRowOrder()
+      ->setMissingRowsLabel(self::missingRowsLabelFor('workflow states'))
+      ->setUnexpectedRowsLabel(self::unexpectedRowsLabelFor('workflow states'))
+      ->assert();
+  }
+
+  /**
+   * Gets all defined workflow objects.
+   *
+   * @return \Drupal\workflows\WorkflowInterface[]
+   *   Returns an array of workflow objects.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   */
+  public function getWorkflows() {
+    /** @var \Drupal\workflows\WorkflowInterface[] $workflows */
+    $workflows = $this->entityTypeManager
+      ->getStorage('workflow')
+      ->loadMultiple();
+    return $workflows;
+  }
+
+}
